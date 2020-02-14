6512f4a10affc6d39ed5d2278f9a7ad7c2a5e192
jjader <jjader03@gmail.com>
2020-02-14 16:00:02 -0300

    Configurando informações básicas do site e css externo

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
 
-AUTHOR = 'NDR Locobots'
+
+AUTHOR = 'Locobots'
+#SITEURL = 'http://yoursite.com'
 SITENAME = 'Locobots'
-SITEURL = ''
+SITETITLE = 'Locobots'
+SITESUBTITLE = 'Núcleo de desenvolvimento de robótica'
+SITEDESCRIPTION = 'Dê asas aos Locos e eles darão vidas às máquinas'
+SITELOGO = 'https://avatars3.githubusercontent.com/u/40442965?s=400&v=4'
+#FAVICON = SITEURL + '/images/favicon.ico'
+RELATIVE_URLS = True
+BROWSER_COLOR = 'black'
+PYGMENTS_STYLE = 'monokai'
+STATIC_PATHS = ['images', 'static']
+CUSTOM_CSS = 'static/custom.css'
 
+
+ROBOTS = 'index, follow'
+
+THEME = 'Flex'
 PATH = 'content'
-
+#OUTPUT_PATH = 'blog/'
 TIMEZONE = 'America/Sao_Paulo'
 
-DEFAULT_LANG = 'pt'
+I18N_TEMPLATES_LANG = 'pt_BR'
+DEFAULT_LANG = 'pt_BR'
+OG_LOCALE = 'pt_BR'
+LOCALE = 'pt_BR'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,17 +38,32 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+PAGES_SORT_ATTRIBUTE = 'date'
 
+LINKS_IN_NEW_TAB = 'external'
+
+# Blogroll
+LINKS = (('BLOG', '/'),)
+         
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('facebook', 'https://www.facebook.com/locobots'),
+          ('instagram', 'https://www.instagram.com/locobots.ufop/'),
+          ('github', 'https://www.github.com/locobots'),
+          ('linkedin', '#')
+          )
+#MENUITEMS = (('Archives', '/archives.html'),
+#             ('Categories', '/categories.html'),
+#     Archives        ('Tags', '/tags.html'),)
+
+#CC_LICENSE = {
+#    'name': 'Creative Commons Attribution-ShareAlike',
+#    'version': '4.0',
+#    'slug': 'by-sa'
+#}
+
+COPYRIGHT_YEAR = datetime.now().year
 
 DEFAULT_PAGINATION = 6
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
